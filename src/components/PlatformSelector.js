9b40380af6bb4c4aef4391647119f0b61c53cdaa
Neel Indap <neelindap@gmail.com>
2021-12-04 10:37:33 -0800

    Rolling logic for platforms

--- src/components/PlatformSelector.js
+++ src/components/PlatformSelector.js
@@ -1,8 +1,21 @@
-import { randomNumber } from '../libs/randomNumberPicker';
+import { useRolling } from "../hook/useRolling";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const PlatformSelector = () => { 
-  const platforms = ['Netflix', 'Amazon Prime', 'Disney+', 'Hulu', 'Apple TV'];
-  return platforms[randomNumber(platforms.length)]; 
-}
+const PlatformSelector = () => {
+  const platforms = ["Netflix", "Amazon Prime", "Disney+", "Hulu", "Apple TV"];
+  const selected = useRolling(platforms);
+
+  return (
+    <TransitionGroup component="span">
+      <CSSTransition
+        classNames="ctr"
+        timeout={{ enter: 100, exit: 100 }}
+        unmountOnExit
+      >
+        <span>{platforms[selected]}</span>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 export default PlatformSelector;
