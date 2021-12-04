84fb11739fbbeb71dc14ce6a5d274643e3e93033
Neel Indap <neelindap@gmail.com>
2021-12-04 10:38:58 -0800

    Rolling logic for Genre selector

--- src/components/GenreSelector.js
+++ src/components/GenreSelector.js
@@ -1,8 +1,30 @@
-import { randomNumber } from '../libs/randomNumberPicker';
+import { useRolling } from "../hook/useRolling";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const GenreSelector = () => { 
-  const genres = ['Horror', 'Action', 'Comedy', 'Romance', 'Drama', 'Sci-fi', 'Thriller', 'Documentary'];
-  return genres[randomNumber(genres.length)]; 
- };
+const GenreSelector = () => {
+  const genres = [
+    "Horror",
+    "Action",
+    "Comedy",
+    "Romance",
+    "Drama",
+    "Sci-fi",
+    "Thriller",
+    "Documentary",
+  ];
+  const selected = useRolling(genres);
+
+  return (
+    <TransitionGroup component="span">
+      <CSSTransition
+        classNames="ctr"
+        timeout={{ enter: 100, exit: 100 }}
+        unmountOnExit
+      >
+        <span>{genres[selected]}</span>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 export default GenreSelector;
